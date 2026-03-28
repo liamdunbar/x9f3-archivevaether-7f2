bffbb05fa6a2ae72d49334c00bc4caaeb7a7acff
liamdunbar <fahlanadityaa@gmail.com>
2026-03-29 06:54:22 +0700

    Update links and usernames in fragments.js

--- fragments/fragments.js
+++ fragments/fragments.js
@@ -3,12 +3,12 @@ const FRAGMENTS = [
     name: "S◇◇◇◇◇",
     username: "@s■■■■■■",
     image: "fragments/assets/satrya.webp",
-    link: "fragments/assets/satrya.html"
+    link: "https://simplefroylan.space/fragments/data/remnant-001"
   },
 
   {
     name: "1◇◇◇◇",
-    username: "@p■■■■■■■■■■■■■■",
+    username: "@p■■■■■■■",
     image: "fragments/assets/1only.webp",
     link: "fragments/assets/1only.html"
   }
