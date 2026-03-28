9ea12b14bce5c66bdc81835a5716718c24a9d2c6
liamdunbar <fahlanadityaa@gmail.com>
2026-03-29 05:48:28 +0700

    Add initial FRAGMENTS data structure

---
+++ fragments/fragments.js
@@ -0,0 +1,15 @@
+const FRAGMENTS = [
+  {
+    name: "S◇◇◇◇◇",
+    username: "@s■■■■■■",
+    image: "fragments/assets/satrya.webp",
+    link: "fragments/assets/satrya.html"
+  },
+
+  {
+    name: "1◇◇◇◇",
+    username: "@p■■■■■■■■■■■■■■",
+    image: "fragments/assets/1only.webp",
+    link: "fragments/assets/1only.html"
+  }
+];
