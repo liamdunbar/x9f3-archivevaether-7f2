676f02549a60fc213820f1a72fb41bf1274af7cd
liamdunbar <fahlanadityaa@gmail.com>
2026-04-12 11:34:01 +0700

    Update fragments.js

--- fragments/fragments.js
+++ fragments/fragments.js
@@ -5,17 +5,18 @@ const FRAGMENTS = [
     image: "/fragments/assets/bio.webp",
     link: "https://simplefroylan.space/fragments/data/remnant-004"
   },
+  {
     name: "R◇◇◇◇◇",
     username: "@m■■■■■■",
     image: "/fragments/assets/rendang.webp",
     link: "https://simplefroylan.space/fragments/data/remnant-003"
   },
+  {
     name: "S◇◇◇◇◇",
     username: "@s■■■■■■",
     image: "/fragments/assets/satrya.webp",
     link: "https://simplefroylan.space/fragments/data/remnant-001"
   },
-
   {
     name: "1◇◇◇◇",
     username: "@p■■■■■■■",
