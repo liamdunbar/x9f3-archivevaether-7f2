ae8f3a4649de5553be2a4823e0795bacbc8c9cb7
liamdunbar <fahlanadityaa@gmail.com>
2026-04-20 19:45:41 +0700

    Add new fragment entries with images and links

--- fragments/fragments.js
+++ fragments/fragments.js
@@ -6,6 +6,18 @@ const FRAGMENTS = [
     link: "https://simplefroylan.space/fragments/data/remnant-003"
   },
   {
+    name: "M◇◇◇◇",
+    username: "@n■■■■■",
+    image: "/fragments/assets/nacho.webp",
+    link: "https://simplefroylan.space/fragments/data/remnant-004"
+  },
+  {
+    name: "F◇◇◇◇",
+    username: "@F■■■■■",
+    image: "/fragments/assets/Fvst.webp",
+    link: "https://simplefroylan.space/fragments/data/remnant-005"
+  },
+  {
     name: "S◇◇◇◇◇",
     username: "@s■■■■■■",
     image: "/fragments/assets/satrya.webp",
